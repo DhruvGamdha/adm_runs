f2e1862e47fd54cc164dd2d0ad70e20fdbe3985b
bglab-dgamdha <dgamdha@iastate.edu>
2024-01-15 20:31:55 -0600

    Refactor clean_and_save_csv function to use separate probe and experiment files

--- NagrajPaper/postprocessing/allData_csvClearning.py
+++ NagrajPaper/postprocessing/allData_csvClearning.py
@@ -11,30 +11,16 @@ import pandas as pd
 import os
 import sys
 
-def clean_and_save_csv(input_file, output_file, time_step=1):
+def clean_and_save_csv(probe_file, exp_file, time_step, output_file):
     # 1. Reading the allData.csv file from the current directory
-    # cwd = os.getcwd()
     startpath = os.path.dirname(os.path.realpath(__file__))
-    df = pd.read_csv(startpath + '/' + input_file)
+    df_probe = pd.read_csv(startpath + '/' + probe_file)
 
     # 2. Removing the initial rows for which the column 6 is zero
-    df = df[df.iloc[:, 5] != 0]
-
-    # Resetting index after removing rows
-    df.reset_index(drop=True, inplace=True)
+    df_probe = df_probe[df_probe.iloc[:, 5] != 0]
+    df_probe.reset_index(drop=True, inplace=True)
     
-    timeCount_col_ind = df.columns.get_loc('Time')
-    
-    timeSec_col_name = 'Time(s)'
-    timeSec_col_ind = timeCount_col_ind + 1
-    
-    timeOffset_col_name = 'Time offset'
-    timeOffset_col_ind = timeSec_col_ind + 1
-    
-    # 3. Adding a column after column 2 with the value from column 2 multiplied with time step parameter
-    df.insert(timeSec_col_ind, timeSec_col_name, df.iloc[:, timeCount_col_ind] * time_step)
-
-    # 4. Adding a column after column 3 with the value from column 3 subtracted by the first value of column 3
+    timeCount_col_ind = df_probe.columns.get_loc('Time')
     
     # Subtracting the time take to print the volume upto the probe location point (30, 0, 4.4), wall size (60, 1.25, 50)
     # Probe is at 6th layer, at the center of the wall, each layer is 0.8 mm tall, 1.25 mm wide, 60 mm long
@@ -42,21 +28,28 @@ def clean_and_save_csv(input_file, output_file, time_step=1):
     # volume upto probe location point = 330 mm^3 ( 60 * 5 + 60 * 0.5)
     # Printer speed = 6.741 mm^3/s
     # Time taken to print upto probe location point = 330 / 6.741 = 48.954 s
+    new_df = pd.DataFrame()
+    new_df[''] = ''     # Add a empty 1st column to new_df 
+    new_df['Time (s)'] = df_probe.iloc[:, timeCount_col_ind] * time_step - 48.954
+    new_df['Temp Sim (K)'] = df_probe.iloc[:, 5]
+    new_df = pd.concat([pd.DataFrame([['', '', '']], columns=new_df.columns), new_df], ignore_index=True)   # Add a empty row at the top of new_df
     
-    df.insert(timeOffset_col_ind, timeOffset_col_name, df.iloc[:, timeSec_col_ind] - 48.954)
-
-    # 5. Saving the new csv file as allData_csvClearning.csv
-    df.to_csv(startpath + '/' + output_file, index=False)
+    # open the output file (csv) and append the new data to it (column wise append)
+    df_exp = pd.read_csv(startpath + '/' + exp_file)
+    df_output = pd.concat([df_exp, new_df], axis=1)
+    
+    df_output.to_csv(startpath + '/' + output_file, index=False)
 
 if __name__ == "__main__":
     
-    # dt = 0.007714
-    dt = 0
+    dt = 0.007714
     if len(sys.argv) > 1:
         dt = float(sys.argv[1])
     else:
         print("Please provide the time step as an argument")
         sys.exit(1)
-    input_file='probeTemp_imple2.csv'
-    output_file='configured_probeTemp_imple2.csv'
-    clean_and_save_csv(input_file, output_file, dt)
+        
+    probe_file='probeTemp_imple2.csv'
+    exp_file='paperData_full.csv'
+    output_file='result.csv'
+    clean_and_save_csv(probe_file, exp_file, dt, output_file)
