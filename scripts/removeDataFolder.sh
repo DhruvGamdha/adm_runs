9b5805894d7ef0f6f7189be6edac992255350146
frontera-dgamdha <dgamdha@iastate.edu>
2024-10-21 15:45:07 -0500

    moved useful bash scripts to scripts folder

---
+++ scripts/removeDataFolder.sh
@@ -0,0 +1,17 @@
+
+rm -rf tests/frontera_run_002/data
+
+
+rm -rf tests/frontera_run_005/data
+rm -rf tests/frontera_run_006/data
+rm -rf tests/frontera_run_007/data
+
+rm -rf tests/frontera_run_009/data
+rm -rf tests/frontera_run_010/data
+rm -rf tests/frontera_run_011/data
+rm -rf tests/frontera_run_012/data
+
+rm -rf tests/frontera_run_014/data
+
+
+rm -rf tests/frontera_run_017/data
