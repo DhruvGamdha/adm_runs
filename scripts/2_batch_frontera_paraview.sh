6c874fe7dd1abbccaf7be1e9d38ff2319ab50c63
bglab-dgamdha <dgamdha@iastate.edu>
2025-05-23 21:08:12 -0500

    Update usage instructions and correct data directory in batch script

--- scripts/2_batch_frontera_paraview.sh
+++ scripts/2_batch_frontera_paraview.sh
@@ -1,11 +1,12 @@
 # Ways to use:
 # Single job
-    # 1. source ~/pvmodules.sh
-    # 2. sh batch_frontera_paraview.sh
+    # 1. Start a ondemand job on: https://tap.tacc.utexas.edu/jobs/ 
+    # 2. source ~/pvmodules.sh
+    # 3. sh batch_frontera_paraview.sh
 
-cd /scratch1/09374/dgamdha/projects/leaphi/adm_runs/tests/frontera_run_015/data
+cd /scratch1/09374/dgamdha/projects/leaphi/adm_runs/tests/frontera_run_001/data
 swr -p 1 pvbatch pvSaveAnimation.py abs
 
-cd /scratch1/09374/dgamdha/projects/leaphi/adm_runs/tests/frontera_run_016/data
-swr -p 1 pvbatch pvSaveAnimation.py abs
+# cd /scratch1/09374/dgamdha/projects/leaphi/adm_runs/tests/frontera_run_016/data
+# swr -p 1 pvbatch pvSaveAnimation.py abs
 
